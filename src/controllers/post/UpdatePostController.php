5e19553977f645b3d1fb3e3b6d85518266681183
nicolascastagna <100592012+nicolascastagna@users.noreply.github.com>
2024-03-08 11:58:35 +0100

    fix bug updatePost

--- src/controllers/post/UpdatePostController.php
+++ src/controllers/post/UpdatePostController.php
@@ -157,7 +157,7 @@ class UpdatePostController
                 $chapo = $chapo !== '' ? $chapo : $fetchPost->chapo;
                 $content = $content !== '' ? $content : $fetchPost->content;
 
-                $success = $postRepository->updatePost($fetchPost->userId, $title, $chapo, $content, $image);
+                $success = $postRepository->updatePost($fetchPost->postId, $title, $chapo, $content, $image);
 
                 if ($success === false) {
                     $error = 'Une erreur est survenue dans la mise à jour de l\'article.';
